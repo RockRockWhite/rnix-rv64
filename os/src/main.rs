dd44ded2ac7ab657bc13a54f690cd14888425afc
RockRockWhite <1834309151@qq.com>
2023-06-19 23:18:21 +0800

    bare mechine

---
+++ os/src/main.rs
@@ -0,0 +1,30 @@
+#![no_std]
+#![no_main]
+
+use core::arch::asm;
+
+mod lang_items;
+
+#[no_mangle]
+extern "C" fn _start() {
+    sys_exit(9);
+}
+
+const SYSCALL_EXIT: usize = 93;
+
+fn syscall(id: usize, args: [usize; 3]) -> isize {
+    let mut ret: isize;
+    unsafe {
+        asm!(
+            "ecall",
+            inlateout("x10") args[0] => ret,
+            in("x11") args[1],
+            in("x12") args[2],
+            in("x17") id);
+    }
+    ret
+}
+
+pub fn sys_exit(xstate: i32) -> isize {
+    syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
+}
